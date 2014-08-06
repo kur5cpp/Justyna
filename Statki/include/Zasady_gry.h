152091e946f4c5e011a53420fa7ea623d1d59f4a
Marcin <kur5cpp@users.noreply.github.com>
2014-08-06 09:21:46 +0200

    :(

---
+++ Statki/include/Zasady_gry.h
@@ -0,0 +1,16 @@
+#ifndef ZASADY_GRY_H
+#define ZASADY_GRY_H
+#include "Plansza.h"
+#include "Gracz.h"
+
+
+class Zasady_gry
+{
+    public:
+        Zasady_gry();
+        virtual ~Zasady_gry();
+    protected:
+    private:
+};
+
+#endif // ZASADY_GRY_H
